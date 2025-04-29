3c914a1c932a60f3319b0037056e40b48aa108b8
Sergio Soares <sergio.veiga.soares@gmail.com>
2025-04-28 21:53:03 -0300

    feat: implements routes in server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import http from 'node:http';
 
 const server = http.createServer((req, res) => {
-  res.end('Hello World\n');
+  if (req.method === 'GET' && req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello, World!');
+  } else if (req.method === 'GET' && req.url === '/api/v1/status') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Status Page');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404 Not Found');
+  }
 });
 
 server.listen(3000, () => {
